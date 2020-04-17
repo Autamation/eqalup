679d09403e4cb3a1d9748595e6fa550da22caee6
Rohitpkt <rohit@prakat.in>
2020-04-17 16:23:42 +0530

    yono

--- src/app/email-registered.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EmailRegistered } from './email-registered';
-
-describe('EmailRegistered', () => {
-  it('should create an instance', () => {
-    expect(new EmailRegistered()).toBeTruthy();
-  });
-});
